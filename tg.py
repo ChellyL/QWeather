e3aa650b623de8f455ba15b7b39cadbdfa93d956
ChellyL <38887991+ChellyL@users.noreply.github.com>
2021-12-18 16:29:46 +0800

    Create tg.py

---
+++ tg.py
@@ -0,0 +1,13 @@
+import telegram
+# import os
+#
+# os.environ['http_proxy'] = 'http://127.0.0.1:7890'
+# os.environ['https_proxy'] = 'https://127.0.0.1:7890'
+# 国内机器建议用代理
+token = '' # 频道token
+userid = '' # 个人id
+
+
+def tgme(words):
+    bot = telegram.Bot(token)
+    bot.sendMessage(chat_id=userid, text=words)
