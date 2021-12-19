5ab1f35e79c794b670553c3aa232c60847065c8c
ChellyL <38887991+ChellyL@users.noreply.github.com>
2021-12-19 12:25:07 +0800

    Update tg.py

--- tg.py
+++ tg.py
@@ -3,10 +3,11 @@ import telegram
 #
 # os.environ['http_proxy'] = 'http://127.0.0.1:7890'
 # os.environ['https_proxy'] = 'https://127.0.0.1:7890'
-# 国内机器建议用代理
+# 国内机器需要用代理
 token = '' # 频道token
 userid = '' # 个人id
 
+# 发送有文字描述的网址： words='<a href="' + wt_link() + '">和风天气首页</a>'
 
 def tgme(words):
     bot = telegram.Bot(token)
